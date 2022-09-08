3d6e4cfba5b138603f359a131cd107a4e6ec6a57
ragoutam <kumarpatragoutam14@gmail.com>
2022-09-08 18:57:17 +0530

    major changes in ui and js code

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-let btn = document.querySelector('#btn');
+var btn = document.querySelector('#btn');
 let floors = document.querySelector('.floors');
 let lifts = document.querySelector('.lifts');
 let systemDiv = document.querySelector('#output');
 
-btn.addEventListener('click',()=>{
 
-floorBuilder(floors.value);
 
-})
+btn.addEventListener('click', () => {
+    floorBuilder(floors.value);
+});
 
-let floorBuilder = (floorNum)=>{
-let floorHtml = ``;
-for(i=floorNum;i>0;i--){
-    floorHtml += `<div class="floor">
+
+let floorBuilder = (floorNum) => {
+    let floorHtml = ``;
+    for (i = floorNum; i > 0; i--) {
+        let index = i;
+        floorHtml += `<div class="floor">
     <div class="buttonDiv">
-    <button>up</button>
-    <button>down</button>
+    <button>⬆️</button>
+    <button>⬇️</button>
     </div>
+    <div class="liftDiv">${liftBuilder(i)}</div>
     </div>`
-}
-systemDiv.innerHTML = floorHtml
+    }
+    systemDiv.innerHTML = floorHtml
 };
+
+
+function liftBuilder(indOfLoop) {
+
+    let liftHtml = ``;
+    for (index = 0; index < lifts.value; index++) {
+        liftHtml += `<div class="lift"></div>`
+    }
+    if (indOfLoop===1) {
+        return liftHtml
+    }
+    else {
+        return '';
+    }
+}
 
 
 
